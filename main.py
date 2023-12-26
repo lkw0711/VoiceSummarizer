348dc9b842b752b07f67dc66449385a9f0df54db
ksx064 <89497242+ksx064@users.noreply.github.com>
2023-12-26 20:57:32 +0800

    tmp fix

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import shutil
 import tempfile
 from openai import OpenAI
 
-tmp_dir = "./tmp"
-audio_path = os.path.join(tmp_dir, audio_file.name)
-shutil.copyfile(audio_file.name, audio_path)
 
-if not os.path.exists(tmp_dir):
-    os.makedirs(tmp_dir)
+tempfile.tempdir = ".\\tmp"
+if not os.path.exists(tempfile.tempdir):
+    os.makedirs(tempfile.tempdir)
+
+input_audio = gr.Audio(sources=["upload", "microphone"],
+                       type="filepath", format="mp3", label="Upload Audio File")
 
 client = OpenAI(
-    api_key = "sess-YiinApevNVx19Audctze9DzuE3yLe04PvtcNGSf5"
+    api_key="sess-YiinApevNVx19Audctze9DzuE3yLe04PvtcNGSf5"
 )
 
 
@@ -36,24 +37,21 @@ def extract_key_points(text):
 
 
 def voicesummarizer(audio_file):
-    # 將上傳的音頻文件保存到臨時位置
-    audio_path = gr.save_file(audio_file, "./audio")
-
     # 調用 transcribe_audio 和 extract_key_points
-    # transcribed_text = transcribe_audio(audio_path)
+    transcribed_text = transcribe_audio(audio_file)
     # key_points = extract_key_points(transcribed_text)
 
-    # return transcribed_text, key_points
+    return transcribed_text
+# , key_points
 
 
 # 創建 gradio 界面
 app = gr.Interface(
     fn=voicesummarizer,
-    inputs=gr.Audio(sources=["upload", "microphone"], type="filepath", format="mp3",
-                    label="Upload Audio File"),
+    inputs=input_audio,
     outputs=[
         gr.Textbox(label="Transcribed Text"),
-        gr.Textbox(label="Key Points")
+        # gr.Textbox(label="Key Points")
     ],
     title="VoiceSummarizer - Audio to Summary",
     description="Upload your audio file and get a transcribed text along with its key points."
@@ -62,5 +60,3 @@ app = gr.Interface(
 if __name__ == "__main__":
     app.launch()
 # share=True
-
-
